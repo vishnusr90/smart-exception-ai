dda9135fd617ec1c5aa15aba2d63b33f746d367b
Vishnu <vishnusr01@gmail.com>
2025-06-02 21:36:49 +0800

    Initial commit

---
+++ src/main/java/com/smart/expection/ai/smart_exception/SmartExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.smart.expection.ai.smart_exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExceptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/expection/ai/smart_exception/SmartExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.expection.ai.smart_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
